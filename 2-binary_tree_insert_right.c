3a6db736068f9d73397a0134f94f81696f48f816
Pascal7601 <levispascal63@gmail.com>
2024-03-27 10:14:00 -0700

    binary trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,29 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - function that inserts a node to the left
+ * binary_tree_insert_right - function that inserts a node to the right
  * @parent: parent node of a node
  * @value: the value to be put in a node
  * Return: returns the address of newnode that has been created
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
+	{
 		return (NULL);
+	}
 
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
-		return (NULL);
-
-	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		return (NULL);
+	}
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
 
-	parent->left = new_node;
+	parent->right = new_node;
 
 	return (new_node);
 }
